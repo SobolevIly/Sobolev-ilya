1b82767efac3a7f6aaf2d6226cef051a54532e73
С <ne0nyar@vk.com>
2024-12-19 12:55:47 +0300

    Add files via upload

---
+++ laba6/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.method1();
+        test.method2();
+        test.method3(14);
+    }
+}
